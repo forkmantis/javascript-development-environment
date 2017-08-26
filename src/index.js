650535ff267dfcba4e0307b75d9c18e37fdbf062
Will Killian <github.user@forkmantis.com>
2017-08-26 08:13:29 -0400

    Update the index.{js,html} files to list users from the API call.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './index.css';
 
-import numeral from 'numeral';
+import {getUsers} from './api/getUsers';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this Awesome course!`); // eslint-disable-line no-console
+// populate table of users from API call
+getUsers().then(result => {
+  let userBody = '';
+
+  result.forEach(user => {
+    userBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="delete-user">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = userBody;
+});
