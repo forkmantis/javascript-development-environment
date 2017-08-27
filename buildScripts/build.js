209dcea05836ffd135da9e71929370607aa59217
Will Killian <github.user@forkmantis.com>
2017-08-27 08:39:19 -0400

    fix error in build.js

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -12,23 +12,22 @@ webpack(webpackConfig).run((err, stats) => {
     console.log(chalk.red(err));
     return 1;
   }
+
+  const jsonStats = stats.toJson();
+
+  if (jsonStats.hasErrors) {
+    return jsonStats.errors.map(error => console.log(chalk.red(error)));
+  }
+
+  if (jsonStats.hasWarings) {
+    console.log(chalk.yellow('Webpack has generated the following warnings:'));
+    jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  // if we got this far, the build succeeded
+  console.log(chalk.green('Your app has been built for production and written to /dist!!!'));
+
   return 0;
 });
-
-const jsonStats = stats.toJson();
-
-if (jsonStats.hasErrors) {
-  return jsonStats.errors.map(error => console.log(chalk.red(error)));
-}
-
-if (jsonStats.hasWarings) {
-  console.log(chalk.yellow('Webpack has generated the following warnings:'));
-  jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
-}
-
-console.log(`Webpack stats: ${stats}`);
-
-// if we got this far, the build succeeded
-console.log(chalk.green('Your app has been built for production and written to /dist!!!'));
-
-return 0;
