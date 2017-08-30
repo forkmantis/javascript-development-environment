3b76f5eed304712f02cb1d5a4bfc1a5f51fe612d
Will Killian <github.user@forkmantis.com>
2017-08-30 16:09:18 -0400

    Remove hardcoded users from distServer, and modify baseUrl to call our heroku site.

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://sheltered-woodland-14934.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
